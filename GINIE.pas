6ca04d174538b3224adeceb041e521867e937f20
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2025-01-09 18:42:14 +0100

    Basis

--- GINIE.pas
+++ GINIE.pas
@@ -4,11 +4,33 @@ Unit GINIE;
 
 Interface
 
+Uses
+   SysUtils, FGL;
+   
+Type 
+	GINIE_StringMap = specialize TFPGMap<string,string>;
+	GINIE_StringArr = specialize TFPGMap<integer,string>;
+	GINIE_ValMap    = specialize TFPGMap<string,GINIE_StringMap>;
+	GINIE_LstMap    = specialize TFPGMap<string,GINIE_StringArr>;
+	TGINIE = Object
+		Private
+			_Values:GINIE_ValMap;
+			_Lists:GINIE_LstMap;
+		Public
+			Constructor Create;
+	End;
+
 Procedure CheckGINIELoaded; // Only used to test the compiler
 
 Implementation
 
 Procedure CheckGINIELoaded; Begin WriteLn('GINIE has been loaded') End;
+
+Constructor TGINIE.Create;
+Begin
+	_Values := GINIE_ValMap.Create;
+	_Lists  := GINIE_LstMap.Create;
+End;
 
 
 End.
