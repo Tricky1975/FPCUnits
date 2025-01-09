c50d0096b8be31560abed6fb6dedf0d53efd4803
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2025-01-09 18:26:59 +0100

    Test code! Time to let the real work begin!

---
+++ GINIE.pas
@@ -0,0 +1,14 @@
+Unit GINIE;
+
+// GINIE Is Not Ini Either
+
+Interface
+
+Procedure CheckGINIELoaded; // Only used to test the compiler
+
+Implementation
+
+Procedure CheckGINIELoaded; Begin WriteLn('GINIE has been loaded') End;
+
+
+End.
